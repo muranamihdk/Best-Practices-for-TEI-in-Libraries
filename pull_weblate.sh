2744bcf686f50b8a0424554cd5052efc80675ac4
Hideaki Muranami <muranamihideaki@gmail.com>
2018-11-26 21:11:27 +0900

    add shell script for pulling translations from weblate

---
+++ pull_weblate.sh
@@ -0,0 +1,32 @@
+cd ~/tei/teiinlibraries/
+echo "git checkout localize_ja"
+git checkout localize_ja
+echo
+
+if [ $(curl \
+ -s \
+ -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+ http://www3420ue.sakura.ne.jp:8080/api/projects/tei-in-libraries/repository/ \
+| jq -r '.needs_commit') = false ]
+then
+  echo "No changes to commit"
+  exit 0
+fi
+
+if [ $(curl \
+ -s \
+ -d operation=commit \
+ -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+ http://www3420ue.sakura.ne.jp:8080/api/components/tei-in-libraries/bptl-driver/repository/ \
+| jq -r '.result') = false ]
+then
+  echo "Commit Failure."
+  exit 1
+else
+  echo "Commit Success."
+fi
+
+echo
+
+echo "git pull origin localize_ja"
+git pull origin localize_ja
