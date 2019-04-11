22d079fe8dbc2e38332820bff53f35127b96cd26
Hideaki Muranami <muranamihideaki@gmail.com>
2019-04-11 20:51:00 +0900

    modified pull_weblate.sh

--- pull_weblate.sh
+++ pull_weblate.sh
@@ -28,5 +28,20 @@ fi
 
 echo
 
+while :
+do
+ if [ $(curl \
+  -s \
+  -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+  http://www3420ue.sakura.ne.jp:8080/api/projects/tei-in-libraries/repository/ \
+ | jq -r '.needs_push') = true ]
+ then
+   echo "Waiting for Weblate to push changes..."
+   sleep 3
+ else
+  break
+ fi
+done
+
 echo "git pull origin localize_ja"
 git pull origin localize_ja
