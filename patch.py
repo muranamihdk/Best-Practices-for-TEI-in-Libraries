28e0964a58843471d98592d7d614f58d6563f162
Hideaki Muranami <muranamihideaki@gmail.com>
2019-12-09 21:39:09 +0900

    modified: patch.py

--- patch.py
+++ patch.py
@@ -5,6 +5,9 @@ replacements = (
     ("http://www.tei-c.org/release/xml/tei/stylesheet/tei-print.css", "https://www.tei-c.org/release/xml/tei/stylesheet/tei-print.css"),
     ("<td>acknowledgement</td><td>（要旨）", "<td>acknowledgement</td><td>（謝辞）"),
     ("<td>foreword</td><td>（要旨）", "<td>foreword</td><td>（序文）"),
+    ("<td>bibliography</td><td>（付録）", "<td>bibliography</td><td>（参考文献）"),
+    ("<td>glossary</td><td>（付録）", "<td>glossary</td><td>（用語集）"),
+    ("<td>notes</td><td>（付録）", "<td>notes</td><td>（注）"),
 )
 
 with open("index.html") as f:
